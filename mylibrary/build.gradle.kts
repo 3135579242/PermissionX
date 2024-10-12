54887d44f4ab921adb617455964c6850ca73b8d0
Lee <3135579242@qq.com>
2024-10-12 17:11:46 +0800

    完成PermissionX库的开发与测试工作

--- mylibrary/build.gradle.kts
+++ mylibrary/build.gradle.kts
@@ -1,20 +1,17 @@
 plugins {
-    alias(libs.plugins.android.application)
+    alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
 }
 
 android {
-    namespace = "com.along.permissionx"
+    namespace = "com.along.mylibrary"
     compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.along.permissionx"
         minSdk = 24
-        targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
     }
 
     buildTypes {
@@ -37,13 +34,9 @@ android {
 
 dependencies {
 
-    implementation(project(":mylibrary"))
-
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
-    implementation(libs.androidx.activity)
-    implementation(libs.androidx.constraintlayout)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
